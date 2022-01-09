3bb3911ffea533c5bad94d901bc0b762a19a5de6
charlie <404749592@qq.com>
2022-01-09 22:14:49 +1100

    init

---
+++ SNH48G_CP_Geneator/src/test/java/com/example/snh48g_cp_geneator/Snh48GCpGeneatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snh48g_cp_geneator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Snh48GCpGeneatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
