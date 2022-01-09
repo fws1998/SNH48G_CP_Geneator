271c3a3a75293c1cd16611a74c9783f24fc59f39
charlie <404749592@qq.com>
2022-01-09 22:06:14 +1100

    init

---
+++ SNH48G_CP_Geneator/src/main/java/com/example/snh48g_cp_geneator/Snh48GCpGeneatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.snh48g_cp_geneator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Snh48GCpGeneatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Snh48GCpGeneatorApplication.class, args);
+    }
+
+}
